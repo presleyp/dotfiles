186e6be3c03d895a54a6a6e1a1794987edf0ef28
G r e y <vapurrmaid@pm.me>
2021-01-07 17:33:26 -0600

    Temporarily comment install.sh

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 
 echo "Hello install.sh"
 
-echo "Clonling home dotfiles..."
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# echo "Clonling home dotfiles..."
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Install HOME-based bash files
-ln -s $DIR/bash/.bash_aliases $HOME
-ln -s $DIR/bash/.bash_path $HOME
-ln -s $DIR/bash/.bashrc $HOME
-ln -s $DIR/bash/.profile $HOME
+# ln -s $DIR/bash/.bash_aliases $HOME
+# ln -s $DIR/bash/.bash_path $HOME
+# ln -s $DIR/bash/.bashrc $HOME
+# ln -s $DIR/bash/.profile $HOME
 
 # Install git files
-ln -s $DIR/git/.gitconfig $HOME
+# ln -s $DIR/git/.gitconfig $HOME
 
 # Install VS Code files
-mkdir -p $HOME/.config/Code/User
-ln -s $DIR/Code/User/keybindings.json $HOME/.config/Code/User
-ln -s $DIR/Code/User/settings.json $HOME/.config/Code/User
+# mkdir -p $HOME/.config/Code/User
+# ln -s $DIR/Code/User/keybindings.json $HOME/.config/Code/User
+# ln -s $DIR/Code/User/settings.json $HOME/.config/Code/User
